c6d0d4bac8d2aaa557293bf376cce4445f2ba4fc
Leandro Melgarejo <leandromelgarejo@hotmail.com>
2022-12-01 22:15:11 -0300

    first commit

---
+++ app/Models/Tag.ts
@@ -0,0 +1,20 @@
+import { DateTime } from 'luxon'
+import { BaseModel, column, HasMany, hasMany } from '@ioc:Adonis/Lucid/Orm'
+import Video from './Video'
+
+export default class Tag extends BaseModel {
+  @column({ isPrimary: true })
+  public id: number
+  
+  @column()
+  public title: string
+
+  @hasMany(() => Video)
+  public videos: HasMany<typeof Video>
+
+  @column.dateTime({ autoCreate: true })
+  public createdAt: DateTime
+
+  @column.dateTime({ autoCreate: true, autoUpdate: true })
+  public updatedAt: DateTime
+}
